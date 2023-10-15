1993c18955c9a18196fcd8b8453378d95082dde0
gpdoud <gpdoud@gmail.com>
2023-10-14 22:22:20 -0400

    finished user change

--- src/app/prs/user/user-change/user-change.component.ts
+++ src/app/prs/user/user-change/user-change.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SystemService } from '../../system/system.service';
+import { UserService } from '../user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../user.class';
 
 @Component({
   selector: 'app-user-change',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class UserChangeComponent {
 
+  pageTitle = "User Change";
+  user: User = new User();
+
+  constructor(
+    private sys: SystemService,
+    private usrsvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  save(): void {
+    this.usrsvc.change(this.user).subscribe({
+      next: (res) => {
+        console.debug("Changed...");
+        this.router.navigateByUrl("/user/lst");
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params["id"];
+    this.usrsvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("User:", res);
+        this.user = res;
+      },
+      error: (err) => console.error(err)
+    });
+  }
 }
