537850ab5e40e33dff2ede84ca7a23af6ead7ad2
gpdoud <gpdoud@gmail.com>
2023-10-14 21:52:39 -0400

    finished user detail

--- src/app/misc/sort/sort.pipe.ts
+++ src/app/misc/sort/sort.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
   transform(arr: any[], col: string, asc: boolean): any[] {
-    const sortFn = (a: any, b: any) => {
-      if (typeof arr === 'undefined' || typeof arr === null) return arr;
+    if (typeof arr === 'undefined' || typeof arr === null) return arr;
+    
+    const sortFn = (a: any, b: any): number => {
       let x = '';
       if (a[col] !== null)
         x = typeof a[col] === 'number' ? a[col] : a[col].toString().toLowerCase();
@@ -22,6 +23,6 @@ export class SortPipe implements PipeTransform {
       }
     };
 
-    return arr.sort();
+    return arr.sort(sortFn);
   }
 }
--- src/app/prs/user/user-detail/user-detail.component.ts
+++ src/app/prs/user/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SystemService } from '../../system/system.service';
+import { UserService } from '../user.service';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { User } from '../user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,4 +11,39 @@ import { Component } from '@angular/core';
 })
 export class UserDetailComponent {
 
+  pageTitle = "User Detail";
+  user: User = new User();
+  
+  constructor(
+    private sys: SystemService,
+    private usrsvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+
+  showVerifyDelete: boolean = false;
+  toggle(): void {
+    this.showVerifyDelete  = !this.showVerifyDelete;
+  }
+
+  remove(): void {
+    this.usrsvc.remove(this.user.id).subscribe({
+      next: (res) => {
+        console.debug("Deleted...");
+        this.router.navigateByUrl("/user/lst");
+      },
+      error: (err) => console.debug(err)
+    });
+  }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.params["id"];
+    this.usrsvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("User:", res);
+        this.user = res;
+      },
+      error: (err) => console.debug(err)
+    });
+  }
 }
