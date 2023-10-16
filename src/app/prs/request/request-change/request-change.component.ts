751f7f7e8ab83fcf9bb0de3f3ac77688ea7f7902
gpdoud <gpdoud@gmail.com>
2023-10-16 06:49:10 -0400

    finished request change

--- src/app/prs/request/request-change/request-change.component.ts
+++ src/app/prs/request/request-change/request-change.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Request } from '../request.class';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from '../../system/system.service';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-change',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class RequestChangeComponent {
 
-}
+  pageTitle = "Request Change";
+  req: Request = new Request();
+
+  constructor(
+    private sys: SystemService,
+    private reqsvc: RequestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  save(): void {
+    this.reqsvc.change(this.req).subscribe({
+      next: (res) => {
+        console.debug("Changed...");
+        this.router.navigateByUrl("/req/lst");
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params["id"];
+    this.reqsvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("Request:", res);
+        this.req = res;
+      },
+      error: (err) => console.error(err)
+    });
+  }}
